9dfd81c4a5f25940f44c8329f42c3eeb97159c41
Felipe Yohan Sapio <ffssyohan@gmail.com>
2026-02-03 14:12:58 -0300

    Added a DELETE category method to service layer

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -17,9 +17,24 @@ public class CategoryServiceImpl implements CategoryService {
         return categories;
     }
 
+
     @Override
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+
+        if (category == null) {
+            return "Category not found.";
+        }
+
+        categories.remove(category);
+        return "Category with categoryId: " + categoryId + " deleted successfully.";
     }
 }
