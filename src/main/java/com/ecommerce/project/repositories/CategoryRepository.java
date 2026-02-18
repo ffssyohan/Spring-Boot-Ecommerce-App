4a1422529b52f1ec7773135968e84dacaaba0a1d
Felipe Yohan Sapio <ffssyohan@gmail.com>
2026-02-18 17:18:15 -0300

    Added a custom method (findByCategoryName) to the JPA respotory and implemented on the service implementation layer

--- src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -4,6 +4,9 @@ import com.ecommerce.project.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // no implementation class necessary as Spring JPA will implement this interfaction in execution
+// unless creating customized queries, like findByCategoryName. JPA analyzes the name of the category and implements it in runtime
+// for that, method names should follow a convention: using findBy tells JPA to use WHERE keyword to build a query that filters the result by something that has to have the exact same name of the field we are using as a filter
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    Category findByCategoryName(String categoryName);
 }
