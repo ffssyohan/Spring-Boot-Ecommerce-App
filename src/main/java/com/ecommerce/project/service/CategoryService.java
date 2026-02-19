69a010d54256d9fb7291df898de1e2676f24b749
Felipe Yohan Sapio <ffssyohan@gmail.com>
2026-02-19 15:02:44 -0300

    Added the DTO layer to the project

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,12 +1,13 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryResponse;
 
 import java.util.List;
 
 //The service layer should be implemented as interface so we can achieve loose coupling and modularity
 public interface CategoryService {
-    List<Category> getAllCategories();
+    CategoryResponse getAllCategories();
     void createCategory(Category category);
     String deleteCategory(Long categoryId);
     Category updateCategory(Category category, Long categoryId);
