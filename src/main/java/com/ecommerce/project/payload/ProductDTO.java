e4d630413f7cf8a7a560c4a74741e1d078900cdd
Felipe Yohan Sapio <ffssyohan@gmail.com>
2026-03-02 15:44:24 -0300

    ProductDTO and Response added to the payload layer

---
+++ src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -0,0 +1,23 @@
+package com.ecommerce.project.payload;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long productId;
+    private String productName;
+    private String image;
+    private Integer quantity;
+    private double price;
+    private double specialPrice;
+    private double discount;
+}
