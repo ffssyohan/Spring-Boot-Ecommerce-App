2da5fd2595272f223c03e5074da2db8f680f8cc4
Felipe Yohan Sapio <ffssyohan@gmail.com>
2026-03-03 11:29:50 -0300

    getProductByCategory method implemented in the service layer

--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -57,4 +57,21 @@ public class ProductServiceImpl implements ProductService {
 
         return productResponse;
     }
+
+    @Override
+    public ProductResponse searchByCategory(Long categoryId) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
+
+        List<Product> products = productRespository.findByCategoryOrderByPriceAsc(category);
+
+        List<ProductDTO> productsDTOs = products.stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .toList();
+
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setContent(productsDTOs);
+
+        return productResponse;
+    }
 }
