779d1dae58124883c4ba68905358b21f1af90e75
Felipe Yohan Sapio <ffssyohan@gmail.com>
2026-04-04 15:07:45 -0300

    Order placemente method implemented in service layer

---
+++ src/main/java/com/ecommerce/project/service/OrderServiceImpl.java
@@ -0,0 +1,116 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.exceptions.APIException;
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
+import com.ecommerce.project.model.*;
+import com.ecommerce.project.payload.OrderDTO;
+import com.ecommerce.project.payload.OrderItemDTO;
+import com.ecommerce.project.repositories.*;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private  PaymentRepository paymentRepository;
+
+    @Autowired
+    private  OrderRepository orderRepository;
+
+    @Autowired
+    private  OrderItemRepository orderItemRepository;
+
+    @Autowired
+    private  ProductRespository productRespository;
+
+    @Autowired
+    private CartService cartService;
+
+    @Override
+    @Transactional
+    public OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage) {
+
+        // getting user cart
+        Cart cart = cartRepository.findCartByEmail(emailId);
+        if (cart == null){
+        // probably thw wrong exception treatment object
+        //throw new APIException("There's no cart be shown");
+            throw new ResourceNotFoundException("Cart", "email", emailId);
+        }
+
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address","addressId",addressId));
+
+
+        // create new order with payment info
+        Order order = new Order();
+        order.setEmail(emailId);
+        order.setOrderDate(LocalDate.now());
+        order.setTotalAmount(cart.getTotalPrice());
+        order.setOrderStatus("Order Accepted.");
+        order.setAddress(address);
+
+        Payment payment = new Payment(paymentMethod, pgPaymentId, pgStatus, pgResponseMessage, pgName);
+        payment.setOrder(order);
+        payment = paymentRepository.save(payment);
+        order.setPayment(payment);
+
+        Order savedOrder = orderRepository.save(order);
+
+        // get items from the cart into the order items
+        List<CartItem> cartItems = cart.getCartItems();
+        if (cartItems.isEmpty()){
+            throw new APIException("Cart is empty.");
+        }
+
+        List<OrderItem> orderItems = new ArrayList<>();
+        for (CartItem cartItem : cartItems){
+
+            OrderItem orderItem = new OrderItem();
+
+            orderItem.setProduct(cartItem.getProduct());
+            orderItem.setQuantity(cartItem.getQuantity());
+            orderItem.setDiscount(cartItem.getDiscount());
+            orderItem.setOrderedProductPrice(cartItem.getProductPrice());
+            orderItem.setOrder(savedOrder);
+            orderItems.add(orderItem);
+        }
+
+        // order items returned as a list of saved ordered items
+        orderItems = orderItemRepository.saveAll(orderItems);
+
+        // update product stock
+        cart.getCartItems().forEach(item -> {
+            int orderedQuantity = item.getQuantity();
+            Product product = item.getProduct();
+            product.setQuantity(product.getQuantity() - orderedQuantity);
+            productRespository.save(product);
+
+            // clear the cart
+            cartService.deleteProductFromCart(cart.getCartId(), item.getProduct().getProductId());
+        });
+
+        // send back the order summary
+        OrderDTO orderDTO = modelMapper.map(savedOrder, OrderDTO.class);
+    orderItems.forEach(item ->
+            orderDTO.getOrderItems()
+                    .add(modelMapper.map(item, OrderItemDTO.class)));
+    orderDTO.setAddressId(addressId);
+
+    return orderDTO;
+    }
+}
